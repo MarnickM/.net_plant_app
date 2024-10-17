931d28a8c99ada42ad5c30849944a1537a976e09
sendewael <r0856234@student.thomasmore.be>
2024-10-17 10:58:34 +0200

    first

---
+++ plantdration/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace plantdration
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ plantdration/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace plantdration
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
