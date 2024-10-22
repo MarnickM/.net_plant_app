3990d8ab1984dcb6708dc4b4d79bf2bd01869454
sendewael <r0856234@student.thomasmore.be>
2024-10-22 13:24:21 +0200

    for you baby

---
+++ plantdration/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace plantdration.Models
+{
+    public class User : ObservableObject
+    {
+        private int id;
+        private string name = string.Empty;
+
+        public int Id
+        {
+            get => id;
+            set => SetProperty(ref id, value);
+        }
+
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        public User(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
+
---
+++ plantdration/Models/UserDataService.cs
@@ -0,0 +1,31 @@
+﻿using plantdration.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plantdration.Models
+{
+    public class UserDataService
+    {
+        public async static Task<List<User>> GetUsersAsync()
+        {
+            return await ApiService<List<User>>.GetAsync("users");
+        }
+        public async static Task InsertUserAsync(User user)
+        {
+            await ApiService<User>.PostAsync("users", user);
+        }
+
+        public async static Task UpdateUserAsync(User user)
+        {
+            await ApiService<User>.PutAsync($"users/{user.Id}", user);
+        }
+
+        public async static Task DeleteUserAsync(User user)
+        {
+            await ApiService<User>.DeleteAsync($"users/{user.Id}");
+        }
+    }
+}
